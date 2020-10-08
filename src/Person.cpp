92b7a0bcb226696db451898283da448805d01019
Ariadna_Ch <ariadna.c2405@gmail.com>
2020-10-08 09:49:37 -0600

    Clase persona

---
+++ src/Person.cpp
@@ -0,0 +1,45 @@
+//
+// Created by ariad on 8/10/2020.
+//
+
+#include "Person.h"
+Person::Person(int id, int age, string name) : id(id), age(age), name(std::move(name)) {}
+
+Person::~Person() = default;
+
+const string &Person::getName() const {
+    return name;
+}
+
+void Person::setName(const string &name) {
+    Person::name = name;
+}
+
+int Person::getId() const {
+    return id;
+}
+
+void Person::setId(int id) {
+    Person::id = id;
+}
+
+int Person::getAge() const {
+    return age;
+}
+
+void Person::setAge(int age) {
+    Person::age = age;
+}
+
+
+
+string Person::toString() const {
+    ostringstream output;
+
+    output<<"Name: " << getName() <<
+          "\nIdentification: " << getId() <<
+          "\nAge: " << getAge();
+
+    return output.str();
+}
+
---
+++ src/Person.h
@@ -0,0 +1,30 @@
+//
+// Created by ariad on 8/10/2020.
+//
+
+#ifndef LAB07_FILES_SERIALIZATION_ARIADNA0024_PERSON_H
+#define LAB07_FILES_SERIALIZATION_ARIADNA0024_PERSON_H
+
+
+class Person {
+private:
+    string name;
+    int id;
+    int age;
+
+public:
+    Person(int id, int age, string name);
+    const string &getName() const;
+    void setName(const string &name);
+    virtual ~Person();
+    int getId() const;
+    void setId(int id);
+    int getAge() const;
+    void setAge(int age);
+    string toString() const;
+
+
+};
+
+
+#endif //LAB07_FILES_SERIALIZATION_ARIADNA0024_PERSON_H
